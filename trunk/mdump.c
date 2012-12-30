c3b1070f0e5bc18e9c758e85d5cba26649ca9f05
aviadr1@gmail.com <aviadr1@gmail.com@d439c99a-009d-a514-763e-daacd10949d5>
2012-12-30 10:59:50 +0000

    remove unsused code

--- trunk/mdump.c
+++ trunk/mdump.c
@@ -308,42 +308,6 @@ static SOCKET initliaze_tcp_socket(mdump_options* opts)
     return sock;
 }
 
-static int udp_socket_create(
-    const char* grouphost, int groupport, 
-    struct sockaddr_storage *addr,
-    socklen_t *addr_len)
-{
-    int udp_fd = -1;
-    struct addrinfo *res0 = NULL, *res = NULL;
-    int family = AF_UNSPEC;
-
-    res0 = udp_resolve_host(NULL, groupport,
-                            SOCK_DGRAM, family, AI_PASSIVE);
-    if (res0 == 0)
-        goto fail;
-    for (res = res0; res; res=res->ai_next) {
-        udp_fd = socket(res->ai_family, SOCK_DGRAM, 0);
-        if (udp_fd != -1) break;
-        //perror("socket");
-    }
-
-    if (udp_fd < 0)
-        goto fail;
-
-    memcpy(addr, res->ai_addr, res->ai_addrlen);
-    *addr_len = res->ai_addrlen;
-
-    freeaddrinfo(res0);
-
-    return udp_fd;
-
- fail:
-    if (udp_fd >= 0)
-        closesocket(udp_fd);
-    if(res0)
-        freeaddrinfo(res0);
-    return -1;
-}
 
 static SOCKET initliaze_udp_socket(mdump_options* opts)
 {
